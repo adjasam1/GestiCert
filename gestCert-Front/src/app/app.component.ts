b8597019d114473c5f2c6420554dfd051eb96879
adjasam1 <samuel.sabot.lp2@gmail.com>
2019-06-06 18:36:42 +0200

    responsive suite

--- gestCert-Front/src/app/app.component.ts
+++ gestCert-Front/src/app/app.component.ts
@@ -1,4 +1,16 @@
 import {Component, OnInit} from '@angular/core';
+import {AddressAlternativeDataService} from './service/address-alternative-data.service';
+import {ApplicationDataService} from './service/application-data.service';
+import {CertificateDataService} from './service/certificate-data.service';
+import {DepartmentDataService} from './service/department-data.service';
+import {EnvironmentDataService} from './service/environment-data.service';
+import {PlateformDataService} from './service/plateform-data.service';
+import {ProfileDataService} from './service/profile-data.service';
+import {RootDataService} from './service/root-data.service';
+import {ServerDataService} from './service/server-data.service';
+import {StatusDemandDataService} from './service/status-demand-data.service';
+import {TypeDemandDataService} from './service/type-demand-data.service';
+import {UserDataService} from './service/user-data.service';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +20,32 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'GestiCert';
 
-  constructor() {
+  constructor(private addressAlternativeDataService: AddressAlternativeDataService,
+              private applicationDataService: ApplicationDataService,
+              private certificateDataService: CertificateDataService,
+              private departmentDataService: DepartmentDataService,
+              private environmentDataService: EnvironmentDataService,
+              private plateformDataService: PlateformDataService,
+              private profileDataService: ProfileDataService,
+              private rootDataService: RootDataService,
+              private serverDataService: ServerDataService,
+              private statusDemandDataService: StatusDemandDataService,
+              private typeDemandDataService: TypeDemandDataService,
+              private userDataService: UserDataService) {
   }
 
   ngOnInit() {
+    this.addressAlternativeDataService.publishAddressAlternative();
+    this.applicationDataService.publishApplication();
+    this.certificateDataService.publishCertificate();
+    this.departmentDataService.publishDepartment();
+    this.environmentDataService.publishEnvironment();
+    this.plateformDataService.publishPlateform();
+    this.profileDataService.publishProfile();
+    this.rootDataService.publishRoot();
+    this.serverDataService.publishServer();
+    this.statusDemandDataService.publishStatusDemand();
+    this.typeDemandDataService.publishTypeDemand();
+    this.userDataService.publishUser();
   }
 }
